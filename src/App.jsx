a563af2c02196808b6555dd21f89f9dd1957dd36
unknown <nyassaleon77@gmail.com>
2025-01-31 20:58:14 +0100

    fourthiest commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,37 @@ import { NewOne } from './components/NewOne'
 function App() {
   const [count, setCount] = useState(0)
 
-const user1 = {
-  name:"doe",
-  surname: "john",
-  tel : "699665522",
-  age : 30,
-  actif : false
-
-}
- const user2 = {
-  name:"dan",
-  surname: "johnny",
-  tel : "6556129023",
-  age : 30,
-  actif : true
-
-}
-const user3 = {
-  name:"Doma",
-  surname: "jay",
-  tel : "6777888999",
-  age : 34,
-  actif : false
-
-}
-
+  const userList = [
+    {
+      name:"doe",
+      surname: "john",
+      tel : "699665522",
+      age : 30,
+      actif : false
+    
+    },
+    {
+      name:"dan",
+      surname: "johnny",
+      tel : "6556129023",
+      age : 30,
+      actif : true
+    
+     },
+    {
+      name:"Doma",
+      surname: "jay",
+      tel : "6777888999",
+      age : 34,
+      actif : false
+    
+    }
+  ]
+ 
   return (
     <>
-      <UserCard UserData={user1} />  <UserCard UserData={user2}/>  <UserCard UserData={user3}/> 
+    {userList.map((user)=>(   <UserCard UserData={user} /> ))}
+   
       <NewOne/>
     </>
   )
